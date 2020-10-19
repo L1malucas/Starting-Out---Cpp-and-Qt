a44d4a7c68db0d2459c315020fb8a974e49df167
Lucas Lima <lucaslima.94@hotmail.com.br>
2020-10-19 18:13:46 -0300

    Soma de dois números!

---
+++ cap2-01-Cpp.cpp
@@ -0,0 +1,17 @@
+//Capítulo 02 - Introdução ao C++, Desafio 01: Soma de dois números!
+
+//Solução para o desafio usando apenas c++.
+
+/*Escreva um programa que armazene duas variáveis integrais (50 e 100), armazene a soma entre elas e exiba o resultado da soma.*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int Cinquenta = 50,
+        Cem = 100,
+        total = Cinquenta + Cem;
+        cout << "Total = " << total << "\n";
+        return 0;
+}
